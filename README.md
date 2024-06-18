5632f461a9e680a023b6b85e786d640295ed75db
Vanderlei Gomes <vanderleigomes.silva@hotmail.com>
2024-06-18 10:02:50 -0300

    Add google auth with next auth and set private route using server side props

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+WIP
+
+Projeto de estudos utilizando NextJS e Next Auth
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import styles from './styles.module.css'
+import { GetServerSideProps } from 'next'
+import { getSession } from 'next-auth/react'
+
+export default function Dashboard(){
+	return(
+		<div className={styles.container}>
+			<Head>
+				<title>Meu painel de tarefas</title>
+			</Head>
+		</div>
+	)
+}
+
+export const getServerSideProps: GetServerSideProps = async ({req}) => {
+	const session = await getSession({req})
+
+	if (!session?.user) return{
+		redirect: {
+			destination : '/',
+			permanent: false
+		}
+	}
+
+	return{
+		props:{}
+	}
+}

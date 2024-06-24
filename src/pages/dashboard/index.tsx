755217f10260ff703136ba40c45d971442b36c1d
Vanderlei Gomes <vanderleigomes.silva@hotmail.com>
2024-06-24 10:11:32 -0300

    Get task from user in database and render on screen. Add delete task function

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps } from "next";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import Head from "next/head";
 
@@ -9,7 +9,9 @@ import { FiShare2 } from "react-icons/fi";
 import { FaTrash } from "react-icons/fa";
 
 import { db } from "../../services/firebaseConnection"
-import { addDoc, collection } from "firebase/firestore"
+import { addDoc, collection, query, orderBy, where, onSnapshot, doc, deleteDoc } from "firebase/firestore"
+
+import Link from "next/link"
 
 interface DashboardProps{
   user: {
@@ -17,9 +19,50 @@ interface DashboardProps{
   }
 }
 
+interface TaskProps {
+  id: string;
+  createdAt: string;
+  public: boolean;
+  task: string;
+  user:  string
+  
+}
+
 export default function Dashboard({ user }: DashboardProps) {
   const [input, setInput] = useState("");
   const [publicTask, setPublicTask] = useState(false);
+  const [tasks, setTasks] = useState<TaskProps[]>([]);
+
+
+  useEffect(()=> {
+    async function loadTasks() {
+      const tasksRef = collection(db, "tasks")
+      const q = query(
+        tasksRef,
+        orderBy("createdAt", "desc"),
+        where("user", "==", user)
+      );  
+
+      onSnapshot(q, (snapshot) => {
+        const list = [] as TaskProps[]
+        snapshot.forEach((doc) => {
+          list.push({
+            id: doc.id,
+            task: doc.data().task,
+            createdAt: doc.data().createdAt,
+            user: doc.data().user,
+            public: doc.data().public,
+
+          })
+        })
+        setTasks(list)
+      })
+    }
+
+
+
+    loadTasks()
+  }, [user?.email])
 
   function handleChangePublic(event: ChangeEvent<HTMLInputElement>) {
     setPublicTask(event.target.checked);
@@ -32,7 +75,7 @@ export default function Dashboard({ user }: DashboardProps) {
 
     try {
       await addDoc(collection(db, "tasks"), {
-       tarefa:  input,
+       task:  input,
        createdAt: new Date(),
        user: user,
        public: publicTask
@@ -44,6 +87,15 @@ export default function Dashboard({ user }: DashboardProps) {
     } catch (error) {
       console.log(error)
     }
+  }
+
+  async function handleShare(id: string){
+    await navigator.clipboard.writeText(`${process.env.NEXT_PUBLIC_URL}/task/${id}`)
+  }
+
+  async function handleDeleteTask(id: string){
+    const docRef = doc(db, "tasks", id)
+    await deleteDoc(docRef)
   }
 
   return (
@@ -81,25 +133,33 @@ export default function Dashboard({ user }: DashboardProps) {
             </form>
           </div>
         </section>
-
         <section className={styles.taskContainer}>
           <h1>Minhas tarefas</h1>
+          {tasks.map((task) => (
+            <article key={task.id} className={styles.task}>
+              {task.public && (
+                <div className={styles.tagContainer}>
+                  <label className={styles.tag}>PUBLICO</label>
+                  <button className={styles.shareButton} onClick={() => handleShare(task.id)}>
+                    <FiShare2 size={22} color="#3183ff" />
+                  </button>
+                </div> 
+              )}
 
-          <article className={styles.task}>
-            <div className={styles.tagContainer}>
-              <label className={styles.tag}>PUBLICO</label>
-              <button className={styles.shareButton}>
-                <FiShare2 size={22} color="#3183ff" />
-              </button>
-            </div>
-
-            <div className={styles.taskContent}>
-              <p>Minha primeira tarefa de exemplo show demais!</p>
-              <button className={styles.trashButton}>
-                <FaTrash size={24} color="#ea3140" />
-              </button>
-            </div>
-          </article>
+              <div className={styles.taskContent}>
+                {task.public ? (
+                  <Link href={`/task/${task.id}`}>
+                    <p>{task.task}!</p>
+                  </Link>
+                ) : (
+                  <p>{task.task}!</p>
+                )}
+                <button className={styles.trashButton} onClick={() => handleDeleteTask(task.id)}>
+                  <FaTrash size={24} color="#ea3140" />
+                </button>
+              </div>
+            </article>
+          ))}
         </section>
       </main>
     </div>
@@ -108,10 +168,8 @@ export default function Dashboard({ user }: DashboardProps) {
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const session = await getSession({ req });
-  // console.log(session);
 
   if (!session?.user) {
-    // Se nao tem usuario vamos redirecionar para  /
     return {
       redirect: {
         destination: "/",
